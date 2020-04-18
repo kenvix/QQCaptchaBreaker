1b2d50627186c22d2d316b4b0e0eb2cc522bc3e7
Kenvix <kenvixzure@live.com>
2020-04-18 12:03:27 +0800

    Init

---
+++ Main/dataset.py
@@ -0,0 +1,63 @@
+import os
+from PIL import Image
+from torch.utils import data
+import numpy as np
+from torchvision import transforms
+import torch
+from torch.utils.data import DataLoader
+from sklearn import preprocessing
+
+
+def getCaptchaDataset(batch_size, root_path):
+    train_dataset = CaptchaDataset(root_path + '/train')
+    train_data_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=0,
+                                   shuffle=True, drop_last=True)
+    test_data = CaptchaDataset(root_path + '/test')
+    test_data_loader = DataLoader(test_data, batch_size=batch_size,
+                                  num_workers=0, shuffle=True, drop_last=True)
+    return train_data_loader, test_data_loader
+
+
+class CaptchaDataset(torch.utils.data.Dataset):
+    def __init__(self, dir_path):
+        super(CaptchaDataset, self).__init__()
+        self.width = 168
+        self.height = 64
+
+        self.images_paths = [os.path.join(dir_path, img) for img in os.listdir(dir_path)]
+        self.transform = transforms.Compose([
+            transforms.Resize((self.height, self.width)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        ])
+
+    def __len__(self):
+        return len(self.images_paths)
+
+    @staticmethod
+    def to_label(string):
+        label = []
+        for i in range(len(string)):
+            label.append(string[i])
+
+        le = preprocessing.LabelEncoder()
+        targets = le.fit_transform(label)
+        return targets
+
+    def __getitem__(self, index):
+        img = self.images_paths[index]
+        label = img.replace('\\', '/').split("/")[-1].split(".")[0]
+        asLabel = self.to_label(label)
+        labelTensor = torch.Tensor(asLabel)
+        datao = Image.open(img)
+        data = self.transform(datao)
+        return data, labelTensor
+
+
+def main():
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ Main/model.py
@@ -0,0 +1,54 @@
+import torch
+from torch import nn
+
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pylab as plt
+import torchvision.models as tvmodels
+from torchvision import datasets, transforms
+import dataset
+
+class CaptchaNN(nn.Module):
+    def __init__(self):
+        super(CaptchaNN, self).__init__()
+        self.kernel_size = 5
+        self.class_num = 26
+
+        self.net = nn.Sequential( #168x64
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=self.kernel_size, padding=(1, 1)),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(6),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=6, out_channels=12, kernel_size=self.kernel_size, padding=(1, 1)),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(12),
+            nn.ReLU(),
+
+            nn.Conv2d(in_channels=12, out_channels=24, kernel_size=self.kernel_size, padding=(1, 1)),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(24),
+            nn.ReLU(),
+        )
+        self.linear = nn.Sequential(
+            nn.Linear(24*6*19, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+        )
+
+        self.fc1 = nn.Linear(128, 62)
+        self.fc2 = nn.Linear(128, 62)
+        self.fc3 = nn.Linear(128, 62)
+        self.fc4 = nn.Linear(128, 62)
+
+    def forward(self, x):
+        x = self.net(x)
+        x = x.view(-1, 24*6*19)
+        x = self.linear(x)
+        x1 = self.fc1(x)
+        x2 = self.fc2(x)
+        x3 = self.fc3(x)
+        x4 = self.fc4(x)
+        return x1, x2, x3, x4
+
